a6b6b5235a15d89e4549e3402f4781b1ddbd4b8e
ManishShee24 <65226145+ManishShee24@users.noreply.github.com>
2021-06-23 09:30:54 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,21 @@
+# Create a Digital Clock using tkinter
+
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root = Tk()
+root.title('Digital Clock')
+
+label = Label(root,font = ('ds-digital',80),background = 'black',
+              foreground = 'cyan')
+
+label.pack(anchor = 'center')
+
+def time():
+    string = strftime('%I : %M : %S %p')
+    label.config(text = string)
+    #label.after(1000,time())
+
+time()
+mainloop()
